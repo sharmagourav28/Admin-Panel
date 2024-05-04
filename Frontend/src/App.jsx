1eea1f6b8edd457fa03b47f5d614fb4943d78572
Gourav <gouravsharma2806@gmail.com>
2024-05-04 19:25:03 +0530

    NIL

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import header from './components/header'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <BrowserRouter>
-        <header />
+
       </BrowserRouter>
 
     </>
